914dad27035997a2eb119530f8f16a561b7c248d
AnirudhHanda <anirudhhanda3@gmail.com>
2022-10-18 22:52:18 +0530

    main

---
+++ JobLe/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '4edb5ed8100f842f0b8dfd0335d3796a6a0337cb',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '4edb5ed8100f842f0b8dfd0335d3796a6a0337cb',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'twig/twig' => array(
+            'pretty_version' => 'v1.13.2',
+            'version' => '1.13.2.0',
+            'reference' => '6d6a1009427d1f398c9d40904147bf9f723d5755',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../twig/twig',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
